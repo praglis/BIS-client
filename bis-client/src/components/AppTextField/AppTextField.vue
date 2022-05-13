83d94c52576015a01fa5178cd0e840d9cafe6498
Pawel <mrbob724@gmail.com>
2022-05-13 19:14:58 +0200

    updateEvent done

--- bis-client/src/components/AppTextField/AppTextField.vue
+++ bis-client/src/components/AppTextField/AppTextField.vue
@@ -1,39 +1,12 @@
 <template>
   <v-text-field
-      v-model="value"
+      v-model="computedActualValue"
       :label="name"
       outlined
       :readonly="disabled"
-      @change="$emit('change', value)"
+      @change="$emit('change', getStringValOrElse([actualValue]))"
   ></v-text-field>
 </template>
 
-<script>
-export default {
-  name: 'app-text-field',
-  components: {},
-  props: {
-    name: {
-      type: String,
-      required: true
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    inValue: {
-      type: String
-    }
-  },
-  data() {
-    return {}
-  },
-  computed: {
-    value() {
-      return this.inValue
-    }
-  },
-  methods: {}
-}
+<script src="./AppTextField.js"></script>
 
-</script>
